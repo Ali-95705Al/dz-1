5801ffb4f49553b7af27e64e72058e1dceda49ba
Ali-95705 <115585105+Ali-95705@users.noreply.github.com>
2023-02-16 22:02:08 +0300

    Task-2П

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -1,15 +1,1 @@
-﻿Console.WriteLine("Введите первое число >");
-int number1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число >");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-if (number1 < number2)
-{
-    Console.WriteLine("Второе число");
-}
-else
-{
-     Console.WriteLine("Первое число >=");
-}
-
+﻿System.Console.WriteLine();
